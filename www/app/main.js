61ce8c4b369bbd69e99f7cff5cd9171fdfa82cb4
Stefan Rimaila <piparkaq@me.com>
2013-09-28 21:07:32 +0300

    Shims etc.

--- www/app/main.js
+++ www/app/main.js
@@ -1,1 +1,22 @@
 
+requirejs.config({
+	baseUrl: '..',
+	shim: {
+		'jquery': {
+			'exports': 'jQuery'
+		}
+	},
+	paths: {
+		'jquery': 'vendor/jquery.min',
+		'bootstrap': 'vendor/bootstrap.min'
+	}
+});
+
+require(
+	['jquery'],
+	function ($) {
+		'use strict';
+
+		// Lel
+	}
+);
